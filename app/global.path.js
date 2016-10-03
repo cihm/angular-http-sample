8ac5a3f4c5f6c086bd0a84b3d41ca6d76b3f36bd
lewisli <lewisli.acer@gmail.com>
2016-10-03 11:07:35 +0800

    rm global

--- app/global.path.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-exports.wsUrl = 'http://210.80.86.148:8080/eInvoiceNumberService/backend/invoiceNotificationThreshold?srcProject=abStoreWeekend&type=triplicateUniformInvoice';
-exports.postUrl = 'http://210.80.86.148:8080/eInvoiceNumberService/backend/invoiceNotificationThreshold';
-//# sourceMappingURL=global.path.js.map
