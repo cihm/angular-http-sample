c30c70bcbfbf3abbc4d9b1e3491e10a10e8f8a36
willy lai@home <willy@csie.io>
2016-10-05 01:01:14 +0800

    ignore config

--- src/app/global.path.ts
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-
-
-
-export var wsUrl: string='http://210.80.86.148:8080/eInvoiceNumberService/backend/invoiceNotificationThreshold?srcProject=abStoreWeekend&type=triplicateUniformInvoice';    
-export var postUrl: string= 'http://210.80.86.148:8080/eInvoiceNumberService/backend/invoiceNotificationThreshold';   
